39b7ba5b13192c1c0281ed5bc2b0238047d96ca0
Makosai <kristopher.ali.dev@gmail.com>
2024-02-10 04:56:25 -0500

    Setup Desktop and Fullstack cargo for Dioxus.

---
+++ common/src/lib.rs
@@ -0,0 +1,84 @@
+#![allow(non_snake_case, unused)]
+#![crate_type = "lib"]
+#![crate_name = "visp_common"]
+
+use dioxus_router::prelude::*;
+use dioxus::prelude::*;
+
+#[cfg(feature = "web")]
+use dioxus_fullstack::prelude::*;
+
+// https://dioxuslabs.com/learn/0.4/router/example/full-code
+#[derive(Routable, PartialEq, Debug, Clone)]
+enum Route {
+    #[layout(NavBar)]
+        #[route("/")]
+        Home {},
+        #[route("/other")]
+        Other {},
+    #[end_layout]
+    //  if the current location doesn't match any of the above routes, render the NotFound component
+    #[route("/:..route")]
+    NotFound { route: Vec<String> },
+}
+
+#[component]
+fn NavBar(cx: Scope) -> Element {
+    render! {
+        nav {
+            ul {
+                li {
+                    Link { to: Route::Home {}, "Home" }
+                }
+                li {
+                    Link { to: Route::Other {}, "Other" }
+                }
+            }
+        }
+        Outlet::<Route> {}
+    }
+}
+
+
+#[component]
+fn Home(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    render! {
+        h1 { "Count me: {count}" }
+        button { onclick: move |_| count += 1, "Increase" }
+        button { onclick: move |_| count -= 1, "Decrease" }
+    }
+}
+
+#[component]
+fn Other(cx: Scope) -> Element {
+    render! {
+        h1 { "This is a temporary second page." }
+    }
+}
+
+#[component]
+fn NotFound(cx: Scope, route: Vec<String>) -> Element {
+    render! {
+        h1 { "Page not found" }
+        p { "We are terribly sorry, but the page you requested doesn't exist." }
+        pre { color: "red", "log:\nattemped to navigate to: {route:?}" }
+    }
+}
+
+pub fn app(cx: Scope) -> Element {
+    render! {
+        Router::<Route> { }
+    }
+}
+
+#[cfg(feature = "desktop")]
+pub fn start_app(app: fn(Scope) -> Element) {
+    dioxus_desktop::launch(app);
+}
+
+#[cfg(feature = "web")]
+pub fn start_app(app: fn(Scope) -> Element) {
+    LaunchBuilder::new(app).launch()
+}
---
+++ fullstack/src/main.rs
@@ -0,0 +1,5 @@
+use visp_common::start_app;
+
+fn main() {
+    start_app(visp_common::app);
+}
