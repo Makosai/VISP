2c703d72b8a4c2aec89044221c87b01edbf78b60
Makosai <kristopher.ali.dev@gmail.com>
2025-09-17 06:04:06 -0400

    Initialize Androrid.

---
+++ src-tauri/gen/android/app/src/main/java/com/quaintstudios/visp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.quaintstudios.visp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
