0aca2973300f91014c995d1496361972f9e72bc7
Makosai <kristopher.ali.dev@gmail.com>
2026-05-23 04:08:35 -0400

    generate and bridge the files

--- packages/visp_bridge/lib/visp_bridge.dart
+++ packages/visp_bridge/lib/visp_bridge.dart
@@ -1,5 +1,4 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library visp_bridge;
+
+export 'src/generated/api.dart';
+export 'src/generated/frb_generated.dart' show RustLib;
